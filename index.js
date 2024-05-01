af3f440ffbc96bb324acd9dcca8a076ee50bca47
CYBER-MD <167730742+CYBER-MD@users.noreply.github.com>
2024-05-01 18:33:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,17 +13,13 @@ const fs = require("fs-extra");
 const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
-╔════◇
-║ *『 WAOW YOU CHOOSE SUHAIL-MD 』*
-║ _You complete first step to making Bot._
-╚════════════════════════╝
-╔═════◇
-║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║ *Ytube:* _youtube.com/SuhailTechInfo_
-║ *Owner:* _https://wa.me/923184474176_
-║ *Note :*_Don't provide your SESSION_ID to_
-║ _anyone otherwise that can access chats_
-╚════════════════════════╝
+『 WOW  CYBER-MD 』
+
+👨‍💻You complete first step to making Bot.
+
+👨‍💻OWNER CYBER-AYESH _https://wa.me/94775341543
+
+👨‍💻 Note :_Don't provide your SESSION_ID
 `
 
 
